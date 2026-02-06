c7e410e099733f2fcb7127e77c92a094875abc53
Pavel5284 <pavel5284@mail.ru>
2026-02-06 23:33:24 +0200

    add recaptcha

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import path from "path"
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  resolve: {
-    alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    // Для PDF файлов сохраняем оригинальное имя
+                    if (assetInfo.name?.endsWith('.pdf')) {
+                        return 'assets/[name][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                }
+            }
+        }
     },
-  },
+    base: "/",
+    resolve: {
+        alias: {
+            "@/": `${path.resolve(__dirname, "src")}/`,
+        },
+    },
 })
